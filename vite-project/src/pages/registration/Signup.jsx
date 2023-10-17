501187f393fd6ae315b997e499a3fadd8193545b
rvjlr <132732024+rvjlr@users.noreply.github.com>
2023-10-17 23:06:23 +0530

    vani

--- vite-project/src/pages/registration/Signup.jsx
+++ vite-project/src/pages/registration/Signup.jsx
@@ -1,6 +1,11 @@
 import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom'
 import myContext from '../../context/data/myContext';
+import { toast } from 'react-toastify';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, fireDB } from '../../firebase/FirebaseConfig';
+import { Timestamp, addDoc, collection } from 'firebase/firestore';
+import Loader from '../../components/loader/Loader';
 
 function Signup() {
     const [name, setName] = useState("");
@@ -43,11 +48,11 @@ function Signup() {
 
     return (
         <div className=' flex justify-center items-center h-screen'>
+            {loading && <Loader/>}
             <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
                 <div className="">
                     <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
                 </div>
-
                 <div>
                     <input type="text"
                         value={name}
@@ -58,12 +63,10 @@ function Signup() {
                     />
                 </div>
 
-
-
                 <div>
                     <input type="email"
                         value={email}
-                        onChange={(e)=> setEmail(e.target.value)}
+                        onChange={(e) => setEmail(e.target.value)}
                         name='email'
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
@@ -73,14 +76,14 @@ function Signup() {
                     <input
                         type="password"
                         value={password}
-                        onChange={(e)=> setPassword(e.target.value)}
+                        onChange={(e) => setPassword(e.target.value)}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
                 </div>
                 <div className=' flex justify-center mb-3'>
                     <button
-                        onClick={Signup}
+                        onClick={signup}
                         className=' bg-red-500 w-full text-white font-bold  px-2 py-2 rounded-lg'>
                         Signup
                     </button>
--- vite-project/src/redux/store.jsx
+++ vite-project/src/redux/store.jsx
@@ -9,6 +9,13 @@ export const store = configureStore({
   
 });
 
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/harshaelon/newr1.git
+//git push -u origin main
+
 
 
 
